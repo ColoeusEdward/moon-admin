9bced12f329b4cfd8536e99b5c2bae552ffa4950
songjian <1778651752@qq.com>
2021-07-12 16:45:01 +0800

    build: 引入naive-ui

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,34 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
   <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <n-config-provider :theme="darkTheme">
+    <n-space>
+      <n-button>Default</n-button>
+      <n-button type="primary">Primary</n-button>
+      <n-button type="info">Info</n-button>
+      <n-button type="success">Success</n-button>
+      <n-button type="warning">Warning</n-button>
+      <n-button type="error">Error</n-button>
+    </n-space>
+  </n-config-provider>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
+import { darkTheme, NSpace, NButton } from 'naive-ui'
 import HelloWorld from './components/HelloWorld.vue'
 
 export default defineComponent({
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    NSpace,
+    NButton
+  },
+  setup() {
+    return {
+      darkTheme
+    }
   }
 })
 </script>
