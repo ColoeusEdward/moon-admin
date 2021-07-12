e349c87d994eec191b8a96adcbc916ea7e0202d4
songjian <1778651752@qq.com>
2021-07-12 22:11:03 +0800

    build: 增加css-render工具包

---
+++ src/components/button/index.tsx
@@ -0,0 +1,24 @@
+import { defineComponent, PropType } from 'vue'
+
+const selectOptions = {
+  text: String as PropType<string> // 按钮文本
+}
+
+const MoonButton = defineComponent({
+  name: 'MoonButton',
+  props: selectOptions,
+  setup() {
+    return {
+
+    }
+  },
+  render() {
+    return (
+      <div class={ `12` }>
+
+      </div>
+    )
+  }
+})
+
+export default MoonButton
