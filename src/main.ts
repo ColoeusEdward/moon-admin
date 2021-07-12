c717cfe996bac8b973eee1cbc097c4c261cb76ca
songjian <1778651752@qq.com>
2021-07-12 17:45:10 +0800

    feat: 引入路由和状态存储

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router'
+import store from './store'
 // 通用字体
 import 'vfonts/Lato.css'
 // 等宽字体
 import 'vfonts/FiraCode.css'
 
 const app = createApp(App)
+app.use(store)
+app.use(router)
 app.mount('#app')
