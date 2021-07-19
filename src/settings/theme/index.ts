23b6f0b6df88936ec1edd2532aa995d422734327
yiumankam <415481084@qq.com>
2021-07-19 17:13:58 +0800

    fix: 完善主题模块导入类型

--- src/settings/theme/index.ts
+++ src/settings/theme/index.ts
@@ -1,8 +1,12 @@
 /**
  * 读取modules里边
  */
+type ThemeOverrideConfig<T> = {
+  [key: string]: T
+}
+
 const files = import.meta.globEager('./modules/*.ts')
-const themeModules: { [key: string]: ModuleTree<unknown> } = {}
+const themeModules: { [key: string]: ThemeOverrideConfig<unknown> } = {}
 Object.keys(files).forEach((path) => {
   const fileName = path.split('/')[2]
   themeModules[fileName.replace(/(\.\/|\.ts)/g, '')] = files[path].default
